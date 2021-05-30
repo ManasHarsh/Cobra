0fad3f7dedc0431a149918030116ecc8cece668f
Manas Harsh <33492814+ManasHarsh@users.noreply.github.com>
2021-05-30 23:31:32 +0530

    Update Cobra.sh

--- Cobra.sh
+++ Cobra.sh
@@ -1,4 +1,3 @@
-
 echo "                          ____ ___  ____  ____      _  "  
 echo "                         / ___/ _ \| __ )|  _ \    / \ "  
 echo "                        | |  | | | |  _ \| |_) |  / _ \  "
@@ -6,6 +5,42 @@ echo "                        | |__| |_| | |_) |  _ <  / ___ \ "
 echo "                         \____\___/|____/|_| \_\/_/   \_\ "
 echo "                       Made with <3 and sweat by ManasH4rsh   "
 echo " 								"
+
+echo "checking your system and setting up the environment: "
+echo ""
+sudo apt-get update && apt-get upgrade;
+sudo apt install golang;
+sudo apt install python3;
+sudo apt install python3-pip;
+sudo apt install git;
+echo "installing your assets, please wait:)"
+echo ""
+
+git clone https://github.com/GerbenJavado/LinkFinder;
+git clone https://github.com/s0md3v/Arjun; 
+git clone https://github.com/m4ll0k/SecretFinder;
+git clone https://github.com/devanshbatham/ParamSpider;
+go get github.com/hakluke/hakrawler;
+GO111MODULE=on go get -u -v github.com/hahwul/dalfox;
+go get github.com/tomnomnom/hacks/waybackurls;
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder;
+GO111MODULE=on go get -v github.com/projectdiscovery/dnsx/cmd/dnsx
+GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei;
+git clone https://github.com/projectdiscovery/nuclei-templates;
+go get github.com/haccer/subjack;
+git clone https://github.com/findomain/findomain.git;
+GO111MODULE=on go get -u -v github.com/lc/gau;
+GO111MODULE=on go get -u -v github.com/hahwul/dalfox;
+go get -v github.com/OWASP/Amass/v3/...;
+git clone https://github.com/maurosoria/dirsearch
+git clone https://github.com/s0md3v/Corsy.git;
+go get -u github.com/ffuf/ffuf;
+GO111MODULE=on go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu;
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx;
+git clone https://github.com/hisxo/gitGraber.git;
+git clone https://github.com/lobuhi/byp4xx.git;
+echo "we are ready! lets go!!"
+echo ""
 
 echo	" 1.  subfinder		2. dnsx				3.  nuclei		"	
 echo	" 4.  httpx       	5. waybackurls            	6.  getallurls					"
@@ -15,7 +50,7 @@ echo	" 13. dalfox		14. findomain			15. arjun"
 echo	" 16. LinkFinder		17. secretfinder		18. ParamSpider"
 echo	" 19. Corsy 		20. gitgraber 			21. byp4xx"
 echo	" 22. Subjack "
-echo	""
+echo    ""
 echo -n "Enter the respective number from tool list that you want to use: "
 read number
 echo ""
